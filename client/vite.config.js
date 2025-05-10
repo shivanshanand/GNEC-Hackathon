74819b35de81019005c57e92ebe07da33f37b684
shiv <74009335+shivanshanand@users.noreply.github.com>
2025-05-10 12:09:55 +0530

    Add files via upload

---
+++ client/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  server:{
+    port:process.env.Port || 3000
+  }
+});
